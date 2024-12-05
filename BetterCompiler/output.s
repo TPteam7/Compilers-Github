3952fd4b010ff75516249952b1232f3a0439ef75
TPteam7 <92225480+TPteam7@users.noreply.github.com>
2024-12-04 23:11:10 -0700

    push fixes. SHOULD WORK A LITTLE

--- BetterCompiler/output.s
+++ BetterCompiler/output.s
@@ -1,9 +1,24 @@
 .data
 	a: .word 0
+	t3: .word 0
 .text
 .globl main
 main:
 	li $t0, 0
+	sw $t0, a
+	lw $t0, a
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	li $v0, 11
+	li $a0, 10
+	syscall
+	li $t0, 0
+	li $t1, 1
+	add $t2, $t0, $t1
+	sw $t2, t3
+	lw $t1, t3
+	move $t0, $t1
 	sw $t0, a
 	lw $t0, a
 	li $v0, 1
